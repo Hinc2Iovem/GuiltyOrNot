99855ab6dbb8473511625837fc470fcbd353bf47
Vitokhin Eldar <hinc2iovem@gmail.com>
2024-03-26 01:32:30 +0600

    feat:login done

--- src/main/java/hinc/come/guiltyornot/api/controllers/AuthController.java
+++ src/main/java/hinc/come/guiltyornot/api/controllers/AuthController.java
@@ -1,8 +1,10 @@
 package hinc.come.guiltyornot.api.controllers;
 
+import hinc.come.guiltyornot.api.exceptions.NotFoundException;
 import hinc.come.guiltyornot.api.exceptions.UserAlreadyExistException;
 import hinc.come.guiltyornot.api.services.AuthService;
 import hinc.come.guiltyornot.store.entities.UserEntity;
+import hinc.come.guiltyornot.store.repositories.UserRepository;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
 
     @Autowired
     AuthService authService;
@@ -33,15 +36,15 @@ public class AuthController {
         }
     }
 
-//    @PostMapping
-//    public ResponseEntity login(@RequestBody UserEntity user) {
-//        try {
-//            authService.registration(user);
-//            return ResponseEntity.ok("User was created successfully");
-//        } catch (UserAlreadyExistException e) {
-//            return ResponseEntity.badRequest().body(e.getMessage());
-//        } catch (Exception e) {
-//            return ResponseEntity.badRequest().body("Something went wrong");
-//        }
-//    }
+    @PostMapping
+    public ResponseEntity login(@RequestBody UserEntity user) {
+        try {
+            UserEntity currentUser = authService.login(user);
+            return ResponseEntity.ok().body(currentUser);
+        } catch (NotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Something went wrong");
+        }
+    }
 }
--- src/main/java/hinc/come/guiltyornot/api/services/AuthService.java
+++ src/main/java/hinc/come/guiltyornot/api/services/AuthService.java
@@ -1,12 +1,16 @@
 package hinc.come.guiltyornot.api.services;
 
+import hinc.come.guiltyornot.api.exceptions.BadRequestException;
 import hinc.come.guiltyornot.api.exceptions.MissingCredentials;
+import hinc.come.guiltyornot.api.exceptions.NotFoundException;
 import hinc.come.guiltyornot.api.exceptions.UserAlreadyExistException;
 import hinc.come.guiltyornot.store.entities.UserEntity;
 import hinc.come.guiltyornot.store.repositories.UserRepository;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -27,5 +31,23 @@ public class AuthService {
             throw new MissingCredentials("All fields required: username, password, role");
         }
         return userRepository.save(user);
+    }
+
+    public UserEntity login(UserEntity user) throws NotFoundException, MissingCredentials, BadRequestException {
+        if (userRepository.findByUsername(user.getUsername()) == null){
+            throw new NotFoundException("Wrong credentials");
+        }
+
+        if (user.getUsername() == null || user.getUsername().isEmpty() ||
+            user.getPassword() == null || user.getPassword().isEmpty()) {
+            throw new MissingCredentials("All fields required: username, password");
+        }
+
+        UserEntity currentUser = userRepository.findByUsername(user.getUsername());
+
+        if(!currentUser.getPassword().equals(user.getPassword())){
+            throw new BadRequestException("Wrong credentials");
+        }
+        return currentUser;
     }
 }
